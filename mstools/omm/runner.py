bc6e6b2f89eaf81029e9c8b84244b2d98f28e4b7
Zheng Gong <unimisia@gmail.com>
2019-11-27 12:46:12 +0100

    clean up OpenMM runner

--- mstools/omm/runner.py
+++ mstools/omm/runner.py
@@ -12,13 +12,19 @@ def slab_correction(system: mm.System):
     muz.addPerParticleParameter('q')
     nbforce = [system.getForce(i) for i in range(system.getNumForces())
                if system.getForce(i).__class__ == mm.NonbondedForce][0]
+    qsum = 0
     for i in range(nbforce.getNumParticles()):
         q = nbforce.getParticleParameters(i)[0].value_in_unit(e)
         muz.addParticle(i, [q])
+        qsum += q
+    if abs(qsum) > 1E-4:
+        raise Exception('Slab correction is not valid for charged system')
+
     box = system.getDefaultPeriodicBoxVectors()
     vol = (box[0][0] * box[1][1] * box[2][2]).value_in_unit(nm ** 3)
     cvforce = mm.CustomCVForce('k*muz*muz')
-    _conv = (1.602E-19) ** 2 / 1E-9 / (4 * 3.1415926) / 8.854E-12 * 6.022E23 / 1000  # convert from e^2/nm to kJ/mol
+    # convert from e^2/nm to kJ/mol
+    _conv = (1.602E-19) ** 2 / 1E-9 / (4 * 3.1415926) / 8.854E-12 * 6.022E23 / 1000
     cvforce.addGlobalParameter('k', 2 * 3.1415926 / vol * _conv)
     cvforce.addCollectiveVariable('muz', muz)
     system.addForce(cvforce)
@@ -34,57 +40,62 @@ def gen_simulation(gro_file, psf_file, prm_file, T, P, tcoupl='langevin', pcoupl
                               constraints=app.HBonds, rigidWater=True,
                               verbose=True)
 
-    if slab:
-        slab_correction(system)
-
     print('Initializing simulation...')
-    if not psf.is_drude:
-        if tcoupl == 'langevin':
+    if tcoupl == 'langevin':
+        if psf.is_drude:
+            print('\tDrude Langevin thermostat: 5.0/ps, 20/ps')
+            integrator = mm.DrudeLangevinIntegrator(T * kelvin, 5.0 / ps, 1 * kelvin, 20 / ps, 0.001 * ps)
+        else:
             print('\tLangevin thermostat: 1.0/ps')
             integrator = mm.LangevinIntegrator(T * kelvin, 1.0 / ps, 0.001 * ps)
-        elif tcoupl == 'nose-hoover':
+    elif tcoupl == 'nose-hoover':
+        if psf.is_drude:
+            print('\tDrude Nose-Hoover thermostat: 10/ps, 40/ps')
+            integrator = mm.DrudeNoseHooverIntegrator(T * kelvin, 10 / ps, 1 * kelvin, 40 / ps, 0.001 * ps)
+        else:
             print('\tNose-Hoover thermostat: 10/ps')
             integrator = mm.NoseHooverIntegrator(T * kelvin, 10 / ps, 0.001 * ps)
     else:
-        if tcoupl == 'langevin':
-            print('\tDual Langevin thermostat: 5.0/ps, 20/ps')
-            integrator = mm.DrudeLangevinIntegrator(T * kelvin, 5.0 / ps, 1 * kelvin, 20 / ps, 0.001 * ps)
-        elif tcoupl == 'nose-hoover':
-            print('\tDual Nose-Hoover thermostat: 10/ps, 50/ps')
-            integrator = mm.DrudeNoseHooverIntegrator(T * kelvin, 5.0 / ps, 1 * kelvin, 20 / ps, 0.001 * ps)
-        else:
-            raise Exception('Available thermostat: langvein, nose-hoover')
-        integrator.setMaxDrudeDistance(0.025 * nm)
+        raise Exception('Available thermostat: langevin, nose-hoover')
+    integrator.setMaxDrudeDistance(0.025 * nm)
 
     if pcoupl == 'iso':
         print('\tIsotropic barostat')
         system.addForce(mm.MonteCarloBarostat(P * bar, T * kelvin, 25))
-    elif pcoupl == 'aniso':
+    elif pcoupl == 'xyz':
         print('\tAnisotropic barostat')
         system.addForce(mm.MonteCarloAnisotropicBarostat([P * bar] * 3, T * kelvin, True, True, True, 25))
     elif pcoupl == 'xy':
-        print('\tAnisotropic Barostat only for X and Y')
+        print('\tAnisotropic barostat only for X and Y')
         system.addForce(mm.MonteCarloAnisotropicBarostat([P * bar] * 3, T * kelvin, True, True, False, 25))
     elif pcoupl == 'z':
-        print('\tAnisotropic Barostat only for Z')
+        print('\tAnisotropic barostat only for Z')
         system.addForce(mm.MonteCarloAnisotropicBarostat([P * bar] * 3, T * kelvin, False, False, True, 25))
-    else:
+    elif pcoupl is None:
         print('\tNo barostat')
+    else:
+        raise Exception('Available barostat: iso, xyz, xy, z, None')
 
+    if slab:
+        print('\tSlab correction applied')
+        slab_correction(system)
+
+    properties = {}
     if platform == 'CUDA':
         print('\tPlatform: CUDA')
-        platform = mm.Platform.getPlatformByName('CUDA')
+        _platform = mm.Platform.getPlatformByName('CUDA')
         properties = {'CudaPrecision': 'mixed'}
     elif platform == 'OpenCL':
         print('\tPlatform: OpenCL')
-        platform = mm.Platform.getPlatformByName('OpenCL')
+        _platform = mm.Platform.getPlatformByName('OpenCL')
         properties = {'OpenCLPrecision': 'mixed'}
-    else:
+    elif platform == 'Reference':
         print('\tPlatform: Reference')
-        platform = mm.Platform.getPlatformByName('Reference')
-        properties = {}
+        _platform = mm.Platform.getPlatformByName('Reference')
+    else:
+        raise Exception('Available platform: CUDA, OpenCL, Reference')
 
-    sim = app.Simulation(psf.topology, system, integrator, platform, properties)
+    sim = app.Simulation(psf.topology, system, integrator, _platform, properties)
     sim.context.setPositions(gro.positions)
     sim.context.setVelocitiesToTemperature(T * kelvin)
     sim.reporters.append(GroReporter('dump.gro', 10000))
