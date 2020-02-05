00973105006201a81ba389e45689b8af51520acc
Zheng Gong <unimisia@gmail.com>
2020-02-05 17:28:08 +0100

    Add support for XYZ file. Add script to generate image charges

---
+++ mstools/trajectory/xyz.py
@@ -0,0 +1,81 @@
+import numpy as np
+from . import Atom, Molecule, Topology, Trajectory, Frame
+
+
+class XYZTopology(Topology):
+    def __init__(self, file):
+        super(XYZTopology, self).__init__()
+        self._file = open(file)
+        n_atom = int(self._file.readline().strip())
+        self.remark = self._file.readline().strip()
+
+        mol = Molecule()
+        self.molecules.append(mol)
+        for i in range(n_atom):
+            words = self._file.readline().split()
+            atom = Atom()
+            atom.id = i
+            atom.type = words[0]
+            mol.add_atom(atom)
+            self.atoms.append(atom)
+
+        self._file.close()
+
+
+class XYZ(Trajectory):
+    def __init__(self, file, mode='r'):
+        super().__init__()
+        self._file = open(file, mode)
+        if mode == 'r':
+            self._get_info()
+
+    def _get_info(self):
+        '''
+        Read the number of atoms and record the offset of lines and frames,
+        so that we can read arbitrary frame later
+        '''
+        try:
+            self.n_atom = int(self._file.readline())
+        except:
+            print('Invalid XYZ file')
+            raise
+        self._file.seek(0)
+
+        self.n_frame = 1
+
+    def read_frame(self, i_frame):
+        if i_frame != 0:
+            raise Exception('Can only read the first frame')
+
+        return self.read_frame_from_string(self._file.read())
+
+    def read_frame_from_string(self, string: str):
+        frame = Frame(self.n_atom)
+        lines = string.splitlines()
+        frame.xlo, frame.xhi = 0, 0
+        frame.ylo, frame.yhi = 0, 0
+        frame.zlo, frame.zhi = 0, 0
+        frame.box = np.array([frame.xhi - frame.xlo, frame.yhi - frame.ylo, frame.zhi - frame.zlo])
+
+        frame.has_charge = False
+
+        for i in range(self.n_atom):
+            words = lines[i + 2].split()
+            frame.positions[i] = np.array(list(map(float, words[1:4])))
+
+        return frame
+
+    def write_frame(self, topology: Topology, frame: Frame, subset=None):
+        self._file.write('%i\n' % topology.n_atom)
+        self._file.write('\n')
+
+        if subset is None:
+            subset = list(range(topology.n_atom))
+        for ii, id in enumerate(subset):
+            atom = topology.atoms[id]
+            position = frame.positions[id]
+            line = '%8s %10.5f %10.5f %10.5f\n' % (atom.type, position[0], position[1], position[2])
+            self._file.write(line)
+
+    def close(self):
+        self._file.close()
---
+++ scripts/gen-imgq.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+'''
+Generate image charge from xyz file
+Used for constant voltage simulation
+'''
+
+import argparse
+from mstools.trajectory.topology import Atom, Molecule
+from mstools.trajectory.xyz import XYZTopology, XYZ
+from mstools.trajectory import Frame
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', required=True, type=str, help='input xyz file')
+parser.add_argument('-o', '--output', required=True, type=str, help='output xyz file')
+parser.add_argument('--cathode', required=True, type=float, help='location (z coordinate) of cathode')
+parser.add_argument('--anode', type=float, help='location (z coordinate) of anode if using Petersen\'s method')
+parser.add_argument('--ignore', default='', type=str, help='ignore these atom types')
+args = parser.parse_args()
+
+top = XYZTopology(args.input)
+trj = XYZ(args.input)
+trj_out = XYZ(args.output, 'w')
+
+print('Topology info: ', top.n_atom, 'atoms;', top.n_molecule, 'molecules')
+print('Trajectory info: ', trj.n_atom, 'atoms;', trj.n_frame, 'frames')
+
+ignore_list = args.ignore.split(',')
+id_atoms = [atom.id for atom in top.atoms if atom.type not in ignore_list]
+
+n_images = len(id_atoms)
+if args.anode is not None:
+    n_images = 2 * len(id_atoms)
+
+frame = trj.read_frame(0)
+frame_out = Frame(top.n_atom + n_images)
+
+for ii in range(top.n_atom):
+    frame_out.positions[ii] = frame.positions[ii]
+
+for ii, id in enumerate(id_atoms):
+    atom = Atom()
+    atom.type = 'IMG'
+    top.atoms.append(atom)
+    top.n_atom += 1
+    frame_out.positions[ii + len(frame.positions)] = frame.positions[id]
+    frame_out.positions[ii + len(frame.positions)][2] = 2 * args.cathode - frame.positions[id][2]
+
+if args.anode is not None:
+    for ii, id in enumerate(id_atoms):
+        atom = Atom()
+        atom.type = 'IMG'
+        top.atoms.append(atom)
+        top.n_atom += 1
+        frame_out.positions[ii + len(frame.positions) + len(id_atoms)] = frame.positions[id]
+        frame_out.positions[ii + len(frame.positions) + len(id_atoms)][2] = 2 * args.anode - frame.positions[id][2]
+
+trj_out.write_frame(top, frame_out)
+trj_out.close()
