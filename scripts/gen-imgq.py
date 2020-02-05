174ff591773cc915d95182dd528c6667879dc7ef
Zheng Gong <unimisia@gmail.com>
2020-02-05 23:38:08 +0100

    Add support for XYZ file. Add script to generate image charges

--- scripts/gen-imgq.py
+++ scripts/gen-imgq.py
@@ -13,8 +13,8 @@ from mstools.trajectory import Frame
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=True, type=str, help='input xyz file')
 parser.add_argument('-o', '--output', required=True, type=str, help='output xyz file')
-parser.add_argument('--anode', required=True, type=float, help='location (z coordinate) of anode')
-parser.add_argument('--cathode', type=float,help='location (z coordinate) of cathode if use Petersen\'s method')
+parser.add_argument('--cathode', required=True, type=float,help='z coordinate of cathode (left electrode)')
+parser.add_argument('--anode', type=float, help='z coordinate of anode (right electrode)')
 parser.add_argument('--ignore', default='', type=str, help='ignore these atom types')
 args = parser.parse_args()
 
@@ -28,32 +28,31 @@ print('Trajectory info: ', trj.n_atom, 'atoms;', trj.n_frame, 'frames')
 ignore_list = args.ignore.split(',')
 id_atoms = [atom.id for atom in top.atoms if atom.type not in ignore_list]
 
-n_images = len(id_atoms)
-if args.cathode is not None:
-    n_images = 2 * len(id_atoms)
+n_atom = top.n_atom
+n_image = len(id_atoms) if args.anode is None else len(id_atoms) * 2
 
 frame = trj.read_frame(0)
-frame_out = Frame(top.n_atom + n_images)
+frame_out = Frame(n_atom + n_image)
 
-for ii in range(top.n_atom):
+for ii in range(n_atom):
     frame_out.positions[ii] = frame.positions[ii]
 
-for ii, id in enumerate(id_atoms):
-    atom = Atom()
-    atom.type = 'IMG'
-    top.atoms.append(atom)
+for ii in range(n_atom):
+    img = Atom()
+    img.type = 'IMG'
+    top.atoms.append(img)
     top.n_atom += 1
-    frame_out.positions[ii + len(frame.positions)] = frame.positions[id]
-    frame_out.positions[ii + len(frame.positions)][2] = 2 * args.anode - frame.positions[id][2]
+    frame_out.positions[ii + n_atom] = frame.positions[id]
+    frame_out.positions[ii + n_atom][2] = 2 * args.cathode - frame.positions[id][2]
 
-if args.cathode is not None:
-    for ii, id in enumerate(id_atoms):
-        atom = Atom()
-        atom.type = 'IMG'
-        top.atoms.append(atom)
+if args.anode is not None:
+    for ii in range(n_atom):
+        img = Atom()
+        img.type = 'IMG'
+        top.atoms.append(img)
         top.n_atom += 1
-        frame_out.positions[ii + len(frame.positions) + len(id_atoms)] = frame.positions[id]
-        frame_out.positions[ii + len(frame.positions) + len(id_atoms)][2] = 2 * args.cathode - frame.positions[id][2]
+        frame_out.positions[ii + n_atom + len(id_atoms)] = frame.positions[id]
+        frame_out.positions[ii + n_atom + len(id_atoms)][2] = 2 * args.anode - frame.positions[id][2]
 
 trj_out.write_frame(top, frame_out)
 trj_out.close()
