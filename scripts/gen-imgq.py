653baa490e12944fdf425429919fb25fc024a16c
Gong Zheng <unimisia@gmail.com>
2020-02-06 00:54:17 +0100

    Add support for XYZ file. Add script to generate image charges

--- scripts/gen-imgq.py
+++ scripts/gen-imgq.py
@@ -16,7 +16,7 @@ parser.add_argument('-o', '--output', required=True, type=str, help='output xyz 
 parser.add_argument('--cathode', required=True, type=float, help='z coordinate of cathode (left electrode)')
 parser.add_argument('--anode', type=float, help='z coordinate of anode (right electrode)')
 parser.add_argument('--ignore', default='', type=str, help='ignore these atom types')
-parser.add_argument('--drude', type=bool, action='store_true', help='generate image for drude particles for heavy atoms')
+parser.add_argument('--drude', action='store_true', help='generate image for drude particles for heavy atoms')
 args = parser.parse_args()
 
 top = XYZTopology(args.input)
@@ -28,7 +28,7 @@ print('Trajectory info: ', trj.n_atom, 'atoms;', trj.n_frame, 'frames')
 
 ignore_list = args.ignore.split(',')
 gen_atoms = [atom for atom in top.atoms if atom.type not in ignore_list]
-hvy_atoms = [atom for atom in gen_atoms if not atom.type.starswith('H')] if args.drude else []
+hvy_atoms = [atom for atom in gen_atoms if not atom.type.startswith('H')] if args.drude else []
 
 n_atom = top.n_atom
 n_image = len(gen_atoms) + len(hvy_atoms)
@@ -39,16 +39,18 @@ with open(args.output, 'w') as f_out:
     f_out.write('%i\n' % (n_atom + n_image))
     f_out.write('simbox with image charges\n')
     for ii, atom in enumerate(top.atoms):
-        pos = frame.positions
+        pos = frame.positions[ii]
         f_out.write('%-8s %10.5f %10.5f %10.5f\n' % (atom.type, pos[0], pos[1], pos[2]))
+    for ii, atom in enumerate(top.atoms):
+        pos = frame.positions[ii]
         if atom in gen_atoms:
-            f_out.write('%-8s %10.5f %10.5f %10.5f\n' % (atom.type, pos[0], pos[1], args.cathode - pos[2]))
+            f_out.write('%-8s %10.5f %10.5f %10.5f\n' % ('IMG', pos[0], pos[1], args.cathode - pos[2]))
         if atom in hvy_atoms:
-            f_out.write('%-8s %10.5f %10.5f %10.5f\n' % (atom.type, pos[0], pos[1], args.cathode - pos[2]))
+            f_out.write('%-8s %10.5f %10.5f %10.5f\n' % ('IMG', pos[0], pos[1], args.cathode - pos[2]))
     if args.anode is not None:
         for ii, atom in enumerate(top.atoms):
-            pos = frame.positions
+            pos = frame.positions[ii]
             if atom in gen_atoms:
-                f_out.write('%-8s %10.5f %10.5f %10.5f\n' % (atom.type, pos[0], pos[1], 2 * args.anode - pos[2]))
+                f_out.write('%-8s %10.5f %10.5f %10.5f\n' % ('IMG', pos[0], pos[1], 2 * args.anode - pos[2]))
             if atom in hvy_atoms:
-                f_out.write('%-8s %10.5f %10.5f %10.5f\n' % (atom.type, pos[0], pos[1], 2 * args.anode - pos[2]))
+                f_out.write('%-8s %10.5f %10.5f %10.5f\n' % ('IMG', pos[0], pos[1], 2 * args.anode - pos[2]))
