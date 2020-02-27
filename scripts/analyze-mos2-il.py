91ffdad79c71e8242838e2a1207655941c18c39f
Zheng Gong <unimisia@gmail.com>
2020-02-27 18:37:17 +0100

    update mos2 script and config file

--- scripts/analyze-mos2-il.py
+++ scripts/analyze-mos2-il.py
@@ -36,7 +36,7 @@ args = parser.parse_args()
 
 eps0 = 8.854188E-12
 q0 = 1.602176E-19
-Ang = 1E-10
+nm = 1E-9
 
 top = Topology.open(args.topology)
 print('Topology info: ', top.n_atom, 'atoms;', top.n_molecule, 'molecules')
@@ -52,7 +52,8 @@ if args.end > trj.n_frame or args.end == -1:
     args.end = trj.n_frame
 
 config = configparser.ConfigParser()
-config.read(args.config)
+if config.read(args.config) == []:
+    raise Exception('config file not exist')
 molecules = config['molecules']['name'].split()
 
 
@@ -96,7 +97,7 @@ def _calc_acf(series):
 
 
 def distribution():
-    bins = np.linspace(0, 70, 701)
+    bins = np.linspace(0, 7, 701)  # nm
     dz = bins[1] - bins[0]
     z_array = (bins[1:] + bins[:-1]) / 2
     charge_array = np.array([0.] * len(z_array))
@@ -111,7 +112,7 @@ def distribution():
     for i in range(args.begin, args.end, args.skip):
         n_frame += 1
         frame = trj.read_frame(i)
-        sys.stdout.write('\r    step %i' % frame.step)
+        sys.stdout.write('\r    frame %i' % i)
         positions = frame.positions
         for mol in top.molecules:
             if mol.name not in molecules:
@@ -169,7 +170,7 @@ def distribution():
     print('')
 
     fig, ax = plt.subplots()
-    ax.set(xlim=[0, 70], xlabel='z (A)', ylabel='particle density (/$A^3$)')
+    ax.set(xlim=[0, 7], xlabel='z (nm)', ylabel='particle density (/$nm^3$)')
     for name, z_list in z_atom_dict.items():
         x, y = histogram(z_list, bins=bins)
         ax.plot(x, y / area / dz / n_frame, label=name)
@@ -178,7 +179,7 @@ def distribution():
     fig.savefig(f'{args.output}-dist.png')
 
     fig, ax = plt.subplots()
-    ax.set(xlim=[0, 70], xlabel='z (A)', ylabel='molecule density (/$A^3$)')
+    ax.set(xlim=[0, 7], xlabel='z (nm)', ylabel='molecule density (/$nm^3$)')
     ax2 = ax.twinx()
     ax2.set_ylabel('cumulative molecule number')
     for name, z_list in z_com_dict.items():
@@ -190,7 +191,7 @@ def distribution():
     fig.savefig(f'{args.output}-dist-com.png')
 
     fig, ax = plt.subplots()
-    ax.set(xlim=[0, 70], xlabel='z (A)', ylabel='charge density (e/$A^3$)')
+    ax.set(xlim=[0, 7], xlabel='z (nm)', ylabel='charge density (e/$nm^3$)')
     ax.plot(z_array, charge_array / area / dz / n_frame, label=' '.join(molecules))
     ax.legend()
     fig.tight_layout()
@@ -233,7 +234,7 @@ def diffusion():
 
     for i in range(args.begin, args.end, args.skip):
         frame = trj.read_frame(i)
-        sys.stdout.write('\r    step %i' % frame.step)
+        sys.stdout.write('\r    frame %i' % i)
 
         t_list.append(frame.step / 1e6)  # ns
         for name, atoms_list in name_atoms_dict.items():
@@ -255,7 +256,7 @@ def diffusion():
     t_array = np.array(t_list) - t_list[0]
     for name, z_series_list in z_dict.items():
         fig, ax = plt.subplots(figsize=(6.4, 12.8))
-        ax.set(ylim=[0, 69.496], xlabel='time (ns)', ylabel='z (A)')
+        ax.set(ylim=[0, 6.95], xlabel='time (ns)', ylabel='z (nm)')
         for z_series in z_series_list:
             ax.plot(t_array, z_series)
         fig.tight_layout()
@@ -266,7 +267,7 @@ def diffusion():
     for name, z_series_list in z_dict.items():
         _len = len(acf_dict[name])
         ax.plot(t_array[:_len], acf_dict[name], label=name)
-    ax.plot([0, t_array[-1] * 0.75], [np.exp(-1), np.exp(-1)], '--', label='$e^{-1}$')
+    ax.plot([0, t_array[-1] * 0.5], [np.exp(-1), np.exp(-1)], '--', label='$e^{-1}$')
     ax.legend()
     fig.tight_layout()
     fig.savefig(f'{args.output}-residence.png')
@@ -275,7 +276,7 @@ def diffusion():
 def voltage():
     frame = trj.read_frame(0)
     area = frame.box[0] * frame.box[1]
-    dz = 0.1
+    dz = 0.01
     n_bin = math.ceil(frame.box[2] / dz)
     bins = np.array([frame.zlo + dz * (i + 0.5) for i in range(n_bin)])
     charges = np.array([0.] * n_bin)
@@ -285,7 +286,7 @@ def voltage():
     for i in range(args.begin, args.end, args.skip):
         n_frame += 1
         frame = trj.read_frame(i)
-        sys.stdout.write('\r    step %i' % frame.step)
+        sys.stdout.write('\r    frame %i' % i)
 
         for k, atom in enumerate(top.atoms):
             if atom.type in ignore_list:
@@ -298,27 +299,27 @@ def voltage():
             charges[i_bin] += q
 
     charges_cumulative = np.cumsum(charges) / n_frame
-    charges /= area * dz * n_frame  # e/A^3
+    charges /= area * dz * n_frame  # e/nm^3
 
     for i in range(1, n_bin):
         s = 0
         for j in range(0, i + 1):
             s += dz * (dz * (i - j)) * charges[j]
-        voltage[i] = -s / eps0 * q0 / Ang
+        voltage[i] = -s / eps0 * q0 / nm
 
     fig = plt.figure(figsize=[6.4, 12.8])
     ax1 = fig.add_subplot('311')
-    ax1.set(xlabel='z (A)', ylabel='charge density (e/A$^3$)')
+    ax1.set(xlabel='z (nm)', ylabel='charge density (e/nm$^3$)')
     ax1.plot(bins, charges)
     ax1.plot(bins, [0] * n_bin, '--')
 
     ax2 = fig.add_subplot('312')
-    ax2.set(ylim=[-5, 5], xlabel='z (A)', ylabel='cumulative charges (e)')
+    ax2.set(ylim=[-5, 5], xlabel='z (nm)', ylabel='cumulative charges (e)')
     ax2.plot(bins, charges_cumulative)
     ax2.plot(bins, [0] * n_bin, '--')
 
     ax3 = fig.add_subplot('313')
-    ax3.set(xlabel='z (A)', ylabel='voltage (V)')
+    ax3.set(xlabel='z (nm)', ylabel='voltage (V)')
     ax3.plot(bins, voltage)
     ax3.plot(bins, [0] * n_bin, '--')
     fig.tight_layout()
@@ -331,7 +332,7 @@ def charge_2d():
         raise Exception('charge_2d function requires charge information in trajectory')
 
     z_span = frame.box[2] / 3
-    _conv = q0 / frame.box[0] / frame.box[1] / Ang ** 2 * 1000  # convert from charge (e) to charge density (mC/m^2)
+    _conv = q0 / frame.box[0] / frame.box[1] / nm ** 2 * 1000  # convert from charge (e) to charge density (mC/m^2)
     ids_cathode = [atom.id for atom in top.atoms if atom.molecule.id == 1]
     qtot_list = []
     for i in range(args.begin, args.end, args.skip):
@@ -342,19 +343,19 @@ def charge_2d():
 
     print('\n%-6i %10.6f %10.6f %10.6f' % (
         len(qtot_list), np.mean(qtot_list) * _conv, np.mean(qtot_list) / len(ids_cathode) * 3,
-        np.mean(qtot_list) * _conv / 1000 * z_span * Ang / eps0))
+        np.mean(qtot_list) * _conv / 1000 * z_span * nm / eps0))
 
 
 def charge_3d():
     frame = trj.read_frame(0)
     area = frame.box[0] * frame.box[1]
     z_span = frame.box[2] / 2
-    _conv = q0 / area / Ang ** 2 * 1000  # convert from charge (e) to charge density (mC/m^2)
+    _conv = q0 / area / nm ** 2 * 1000  # convert from charge (e) to charge density (mC/m^2)
     ids_cathode = [atom.id for atom in top.atoms if atom.molecule.id == 1]
     qtot_list = []
     for i in range(args.begin, args.end, args.skip):
         frame = trj.read_frame(i)
-        qtot = args.voltage * area / z_span * eps0 / q0 * Ang
+        qtot = args.voltage * area / z_span * eps0 / q0 * nm
         for ii, atom in enumerate(top.atoms):
             if atom.molecule.name == 'MoS2' or atom.type == 'IMG':
                 continue
@@ -366,7 +367,7 @@ def charge_3d():
 
     print('\n%-6i %10.6f %10.6f %10.6f' % (
         len(qtot_list), np.mean(qtot_list) * _conv, np.mean(qtot_list) * _conv / len(ids_cathode) * 3,
-        np.mean(qtot_list) * _conv / 1000 * z_span * Ang / eps0))
+        np.mean(qtot_list) * _conv / 1000 * z_span * nm / eps0))
 
 
 if __name__ == '__main__':
