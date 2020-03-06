74cf7757926a58904ad5eef9032f2f30556280bf
Zheng Gong <unimisia@gmail.com>
2020-03-06 17:59:44 +0100

    fix cycle import

--- mstools/forcefield/typing.py
+++ mstools/forcefield/typing.py
@@ -1,15 +1,12 @@
-from ..topology import Topology, Molecule
-
-
 class TypingEngine():
     def __init__(self):
         pass
 
-    def type(self, top: Topology):
-        for mol in top.molecules:
+    def type(self, topology):
+        for mol in topology.molecules:
             self.type_molecule(mol)
 
-    def type_molecule(self, mol: Molecule):
+    def type_molecule(self, molecule):
         '''
         Should be implemented by inheritor
         :param mol:
