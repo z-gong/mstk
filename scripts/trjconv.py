cd32899c535d519b727216d697149379121ad12a
Zheng Gong <unimisia@gmail.com>
2020-03-17 22:05:42 +0100

    fix the performance of trajectory reader

--- scripts/trjconv.py
+++ scripts/trjconv.py
@@ -15,17 +15,16 @@ parser.add_argument('-o', '--output', required=True, type=str, help='output gro 
 parser.add_argument('-b', '--begin', default=0, type=int, help='first frame to output')
 parser.add_argument('-e', '--end', default=-1, type=int, help='last frame to output')
 parser.add_argument('--skip', default=1, type=int, help='skip frames between output')
-parser.add_argument('--topignore', default='', type=str,
+parser.add_argument('--topignore', nargs='+', type=str,
                     help='ignore these molecule types in topology in case topology and trajectory do not match')
-parser.add_argument('--ignore', default='', type=str, help='ignore these molecule types')
+parser.add_argument('--ignore', nargs='+', type=str, help='ignore these molecule types')
 parser.add_argument('--box', nargs=3, default=[-1, -1, -1], type=float, help='overwrite the box dimensions')
 parser.add_argument('--shift', nargs=3, default=[0, 0, 0], type=float, help='shift the positions of all atoms')
 args = parser.parse_args()
 
 _top = Topology.open(args.topology)
-top_ignore_list = args.topignore.split(',')
 top = Topology()
-top.init_from_molecules([mol for mol in _top.molecules if mol.name not in top_ignore_list])
+top.init_from_molecules([mol for mol in _top.molecules if mol.name not in args.topignore])
 print('Topology info: ', top.n_atom, 'atoms;', top.n_molecule, 'molecules')
 trj = Trajectory.open(args.input)
 print('Trajectory info: ', trj.n_atom, 'atoms;', trj.n_frame, 'frames')
@@ -35,9 +34,8 @@ if (top.n_atom != trj.n_atom):
 
 trj_out = Trajectory.open(args.output, 'w')
 
-ignore_list = args.ignore.split(',')
-if ignore_list != []:
-    subset = [atom.id for atom in top.atoms if atom.molecule.name not in ignore_list]
+if args.ignore != []:
+    subset = [atom.id for atom in top.atoms if atom.molecule.name not in args.ignore]
 else:
     subset = None
 
