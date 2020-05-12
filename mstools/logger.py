7252443afd35207dc30f6dca4608008dbe1c6dda
Zheng Gong <unimisia@gmail.com>
2020-05-12 14:33:02 +0200

    fix logging

--- mstools/logger.py
+++ mstools/logger.py
@@ -1,10 +1,9 @@
 import logging
 
-logging.captureWarnings(True)
 logger = logging.getLogger('mstools')
+logger.setLevel(logging.INFO)
 # formatter = logging.Formatter('%(asctime)s    %(levelname)-10s %(message)s')
 formatter = logging.Formatter('%(levelname)-10s %(message)s')
-screen_handler = logging.StreamHandler()
-screen_handler.setLevel(logging.INFO)
-screen_handler.setFormatter(formatter)
-logger.addHandler(screen_handler)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger.addHandler(handler)
