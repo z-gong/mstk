5d7d749b93ad96e08c84e65bff34ce8c65c09ad8
Zheng Gong <unimisia@gmail.com>
2020-07-15 15:42:34 +0200

    clean up formula parser. update MoS2 script

--- mstools/formula.py
+++ mstools/formula.py
@@ -1,4 +1,4 @@
-class Formula():
+class Formula:
     '''
     Parse elements and numbers from a non-standardized chemical formula.
 
@@ -7,25 +7,18 @@ class Formula():
     Parameters
     ----------
     mol_str : str
-    
+
     Attributes
     ----------
-    atomlist : list of tuple
-        Each element of this list is a tuple represents the symbol and number of each chemical element.
-    atomdict : dict, [str, int]
+    atoms : dict, [str, int]
         Dict of the symbol and number of each chemical element.
     '''
-    def __init__(self, mol_str=None):
-        self.atomlist = []  # list of (atom, number)
-        self.atomdict = {}
-        if mol_str is not None:
-            self._load(mol_str)
 
-    def _load(self, mol_str):
-        token_list = self._get_token(mol_str)
-        self._calculate(token_list)
-        self._count()
-        self.atomlist = self._sort_hill()
+    def __init__(self, mol_str):
+        chars = Formula._extract_chars(mol_str)
+        chars = Formula._expand_chars(chars)
+        atoms = Formula._count_atoms(chars)
+        self.atoms = Formula._hill_order(atoms)
 
     @staticmethod
     def read(mol_str):
@@ -40,78 +33,109 @@ class Formula():
         -------
         formula : Formula
         '''
-        mol = Formula()
-        token_list = mol._get_token(mol_str)
-        mol._calculate(token_list)
-        mol._count()
-        mol.atomlist = mol._sort_hill()
-        return mol
+        return Formula(mol_str)
 
-    def _get_token(self, mol_str):
-        tmp = ''
-        tmp_num = ''
-        token_list = []
-
-        for i in mol_str:
-            if i in ['(', ')']:
-                if tmp:
-                    token_list.append(tmp)
-                    tmp = ''
-                if tmp_num:
-                    token_list.append(tmp_num)
-                    tmp_num = ''
-                elif token_list and token_list[-1] == ')':
-                    token_list.append('1')
-
-                token_list.append(i)
-            elif i.isdigit():
-                if tmp:
-                    token_list.append(tmp)
-                    tmp = ''
-                tmp_num += i
-
+    @staticmethod
+    def _extract_chars(formula):
+        chars = []
+        i = 0
+        while i < len(formula):
+            c = formula[i]
+            if c.isupper():
+                chars.append(c)
+                i += 1
+                while i < len(formula):
+                    if formula[i].islower():
+                        chars[-1] += formula[i]
+                        i += 1
+                    else:
+                        break
+            elif c.isdigit():
+                chars.append(c)
+                i += 1
+                while i < len(formula):
+                    if formula[i].isdigit():
+                        chars[-1] += formula[i]
+                        i += 1
+                    else:
+                        break
+            elif c == '(' or c == ')':
+                chars.append(c)
+                i += 1
             else:
-                if tmp_num:
-                    token_list.append(tmp_num)
-                    tmp_num = ''
-                elif token_list and token_list[-1] == ')':
-                    token_list.append('1')
-                if i.isupper():
-                    if tmp:
-                        token_list.append(tmp)
-                    tmp = i
+                raise Exception('Invalid character: %s' % c)
+
+        if chars[0].isdigit():
+            raise Exception('Invalid formula')
+        if chars.count('(') != chars.count(')'):
+            raise Exception('Unmatched brackets')
+        return chars
+
+    @staticmethod
+    def _expand_chars(chars):
+        chars = chars[:]
+        temp_expanded_chars = []
+        temp_chars = []
+        while '(' in chars:
+            _start = False
+            i = 0
+            while i < len(chars):
+                c = chars[i]
+                if c == '(':
+                    _start = True
+                    temp_expanded_chars = chars[:i]
+                    temp_chars = []
+                    i += 1
+                elif c == ')':
+                    if not _start:
+                        raise Exception('Unmatched brackets')
+                    if i + 1 < len(chars) and chars[i + 1].isdigit():
+                        n = int(chars[i + 1])
+                        temp_expanded_chars += temp_chars * n
+                        if i + 2 < len(chars):
+                            temp_expanded_chars += chars[i + 2:]
+                    else:
+                        n = 1
+                        temp_expanded_chars += temp_chars * n
+                        if i + 1 < len(chars):
+                            temp_expanded_chars += chars[i + 1:]
+                    chars = temp_expanded_chars
+                    break
                 else:
-                    tmp += i
-        if tmp:
-            token_list.append(tmp)
-        if tmp_num:
-            token_list.append(tmp_num)
-        elif token_list and token_list[-1] == ')':
-            token_list.append('1')
+                    if _start:
+                        temp_chars.append(c)
+                    i += 1
 
-        return token_list
+        return chars
 
-    def _calculate(self, token_list):
-        for token in token_list:
-            if token == '(':
-                self.atomlist.append(('(', 0))
-            elif token == ')':
-                tmp_list = []
-                while self.atomlist[-1][0] != '(':
-                    tmp_list.append(self.atomlist.pop())
-                self.atomlist.pop()
-                self.atomlist.append((tmp_list, 1))
-            elif token.isdigit():
-
-                if isinstance(self.atomlist[-1][0], list):
-                    tmp_list = self.atomlist.pop()[0]
-                    for atom, cnt0 in tmp_list:
-                        self.atomlist.append((atom, cnt0 * int(token)))
-                else:
-                    atom, cnt0 = self.atomlist.pop()
-                    self.atomlist.append((atom, cnt0 * int(token)))
+    @staticmethod
+    def _count_atoms(chars):
+        counts = {}
+        for i in range(len(chars)):
+            c = chars[i]
+            if c.isdigit():
+                counts[chars[i - 1]] += int(c) - 1
             else:
-                self.atomlist.append((token, 1))
+                if c not in counts:
+                    counts[c] = 1
+                else:
+                    counts[c] += 1
+        return counts
+
+    @staticmethod
+    def _hill_order(atoms):
+        count_C = atoms.pop('C') if 'C' in atoms else 0
+        count_H = atoms.pop('H') if 'H' in atoms else 0
+        symbols = list(atoms.keys())
+        symbols.sort()
+        atoms_sorted = {}
+        if count_C != 0:
+            atoms_sorted['C'] = count_C
+        if count_H != 0:
+            atoms_sorted['H'] = count_H
+        for s in symbols:
+            atoms_sorted[s] = atoms[s]
+        return atoms_sorted
 
     def to_str(self):
         '''
@@ -121,41 +145,14 @@ class Formula():
         -------
         formula_str : str
         '''
-        return ''.join([name + Formula._to_num(num) for name, num in self.atomlist])
 
-    def _count(self):
-        for name, num in self.atomlist:
-            if name not in self.atomdict:
-                self.atomdict[name] = num
+        def num2str(num):
+            if num == 1:
+                return ''
             else:
-                self.atomdict[name] += num
+                return str(num)
 
-    def _sort_hill(self):
-        outlist = []
-        C_cnt = None
-        H_cnt = None
-        if 'C' in self.atomdict:
-            outlist.append(('C', self.atomdict['C']))
-            C_cnt = self.atomdict['C']
-            del self.atomdict['C']
-            if 'H' in self.atomdict:
-                outlist.append(('H', self.atomdict['H']))
-                H_cnt = self.atomdict['H']
-                del self.atomdict['H']
-
-        outlist += list(sorted(self.atomdict.items(), key=lambda x: x[0]))
-        if C_cnt:
-            self.atomdict['C'] = C_cnt
-        if H_cnt:
-            self.atomdict['H'] = H_cnt
-        return outlist
-
-    @staticmethod
-    def _to_num(num):
-        if num == 1:
-            return ''
-        else:
-            return str(num)
+        return ''.join([symbol + num2str(num) for symbol, num in self.atoms.items()])
 
     @property
     def n_heavy(self):
@@ -167,7 +164,7 @@ class Formula():
         n_heavy : int
         '''
         n = 0
-        for k, v in self.atomdict.items():
+        for k, v in self.atoms.items():
             if k != 'H':
                 n += v
         return n
@@ -193,7 +190,31 @@ class Formula():
         n_h : int
         '''
         n = 0
-        for k, v in self.atomdict.items():
+        for k, v in self.atoms.items():
             if k == 'H':
                 n += v
         return n
+
+    @property
+    def atomdict(self):
+        '''
+        Alias of :attr:`atoms`, exist for compatibility concern.
+
+        Returns
+        -------
+        atoms : dict, [str, int]
+            Dict of the symbol and number of each chemical element.
+        '''
+        return self.atoms
+
+    @property
+    def atomlist(self):
+        '''
+        Get the symbol and number of each chemical element, exist for compatibility concern.
+
+        Returns
+        -------
+        atomlist : list of tuple
+            Each element of this list is a tuple represents the symbol and number of each chemical element.
+        '''
+        return list(self.atoms.items())
