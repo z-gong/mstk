491bb2b2f483f22c80f6d1eea962184f0f685abb
Zheng Gong <unimisia@gmail.com>
2021-02-14 21:10:43 +0100

    improve performance by vectorization

--- scripts/analyze-mof-il.py
+++ scripts/analyze-mof-il.py
@@ -43,9 +43,10 @@ idx_sol = np.array([atom.id for atom in top.atoms if atom.molecule.name != 'ZIF8
 idx_zn1 = np.array([atom.id for atom in top.atoms if atom.symbol == 'Zn' and atom.molecule.id == 0])
 idx_zn2 = np.array([atom.id for atom in top.atoms if atom.symbol == 'Zn' and atom.molecule.id == 1])
 idx_p = np.array([atom.id for atom in top.atoms if atom.symbol == 'P' and atom.molecule.name == 'P66614'])
+idx_o = np.array([atom.id for atom in top.atoms if atom.symbol == 'O' and atom.molecule.name == 'NTF2'])
 idx_n = np.array([atom.id for atom in top.atoms if atom.symbol == 'N' and atom.molecule.name == 'NTF2'])
-idx_o = np.array([atom.id for atom in top.atoms if atom.symbol == 'O' and atom.molecule.name == 'PEG500'])
-idx_list = [idx_p, idx_n, idx_o]
+idx_e = np.array([atom.id for atom in top.atoms if atom.symbol == 'O' and atom.molecule.name == 'PEG500'])
+idx_list = [idx_p, idx_o, idx_n, idx_e]
 
 print(len(idx_zn1), len(idx_zn2))
 print([len(l) for l in idx_list])
@@ -54,6 +55,21 @@ trj = Trajectory.open(args.input)
 
 if args.end > trj.n_frame or args.end == -1:
     args.end = trj.n_frame
+
+
+def wrap_pbc(positions, origin, cell):
+    displace_array = positions - origin
+    delta_array = np.floor(displace_array / cell.lengths + 0.5)
+    return positions - delta_array * cell.lengths
+
+
+def cartesian2cylindrical(positions, origin, vec_unit):
+    vec_relative = positions - origin
+    z_array = vec_relative.dot(vec_unit)
+    rsq_array = np.sum(vec_relative * vec_relative, axis=1) - z_array ** 2
+    rsq_array = np.maximum(rsq_array, 0)
+    r_array = rsq_array ** 0.5
+    return r_array, z_array
 
 
 def calc_rsq(xyz1, xyz2):
@@ -72,8 +88,8 @@ def calc_r_z(vec, vec_unit):
 
 dr = args.dr
 max_r = 3
-max_z = 5
-min_z = -5
+max_z = 4.5
+min_z = -4.5
 r_list = [(i + 0.5) * dr for i in range(int(max_r / dr))]
 z_list = [(i + 0.5) * dr + min_z for i in range(int((max_z - min_z) / dr))]
 densities = [np.zeros([len(z_list), len(r_list)]) for l in idx_list]
@@ -101,28 +117,25 @@ for i in range(args.begin, args.end, args.skip):
     # print(com_zn1, com_zn2, vec, vec_unit)
 
     # consider PBC
-    for mol in top.molecules:
-        pos_mol = frame.positions[[atom.id for atom in mol.atoms]]
-        com_mol = np.mean(pos_mol, axis=0)
-        displace = com_mol - com_zn
-        delta = np.floor(displace / frame.cell.lengths + 0.5)
-        frame.positions[[atom.id for atom in mol.atoms]] -= delta * frame.cell.lengths
+    positions = wrap_pbc(frame.positions, com_zn, frame.cell)
 
     if args.cmd == 'dist':
         for idx, density in zip(idx_list, densities):
+            if len(idx) == 0:
+                continue
             rho = len(idx) / frame.cell.volume
-            for i in idx:
-                vec = frame.positions[i] - com_zn
-                _r, _z = calc_r_z(vec, vec_unit)
+            r_array, z_array = cartesian2cylindrical(positions[idx], com_zn, vec_unit)
+            for ii, i in enumerate(idx):
+                _r, _z = r_array[ii], z_array[ii]
                 if _r < max_r and _z < max_z and _z > min_z:
                     idx_r = int(_r / dr)
                     density[int((_z - min_z) / dr)][idx_r] += 1 / (2 * np.pi * r_list[idx_r] * dr * dr) / rho
 
     elif args.cmd == 'charge':
         for idx, density in zip([idx_zif, idx_sol], density_charges):
-            for i in idx:
-                vec = frame.positions[i] - com_zn
-                _r, _z = calc_r_z(vec, vec_unit)
+            r_array, z_array = cartesian2cylindrical(positions[idx], com_zn, vec_unit)
+            for ii, i in enumerate(idx):
+                _r, _z = r_array[ii], z_array[ii]
                 if _r < max_r and _z < max_z and _z > min_z:
                     idx_r = int(_r / dr)
                     density[int((_z - min_z) / dr)][idx_r] += charges[i] / (2 * np.pi * r_list[idx_r] * dr * dr)
@@ -141,7 +154,7 @@ elif args.cmd == 'charge':
     for i, density in enumerate(density_charges + [np.sum(density_charges, axis=0)]):
         fig, ax = plt.subplots(1, 1, figsize=(5, 10))
         ax.set(xlim=[0, max_r], ylim=[min_z, max_z])
-        im = ax.pcolormesh(r_list, z_list, density / n_frame, shading='nearest', cmap='PiYG', vmin=-20, vmax=20)
+        im = ax.pcolormesh(r_list, z_list, density / n_frame, shading='nearest', cmap='PiYG', vmin=-4, vmax=4)
         fig.colorbar(im, ax=ax)
         fig.savefig('dist-charge-%i.png' % i)
         if platform.system() != 'Linux':
