Fix energy kernels for torsions. Angle and torsion evaluation gives radian instead of degree.

mstools/analyzer/energy_kernels.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .. import constant
 
 
 def ew_dot(vec1, vec2):
@@ -58,7 +59,7 @@ class HarmonicAngleKernel():
         self.a2 = self.indexes[:, 1]
         self.a3 = self.indexes[:, 2]
         self.parameters = np.array(parameters, dtype=np.float64)
-        self.theta0 = self.parameters[:, 0] / 180 * np.pi
+        self.theta0 = self.parameters[:, 0]
         self.k = self.parameters[:, 1]
 
     def evaluate(self):
@@ -92,7 +93,7 @@ class HarmonicAngleKernel():
         return theta, energy, forces
 
 
-class PeriodicTorsionKernel():
+class OplsTorsionKernel():
     '''
     E = k1 (1+cos(phi)) + k2 (1-cos(2 phi)) + k3 (1+cos(3 phi)) + k4 (1-cos(4 phi))
 
@@ -136,17 +137,18 @@ class PeriodicTorsionKernel():
         energy = c0 + c1 * cos + c2 * cos2 + c3 * cos3 + c4 * cos4
 
         sin = np.sin(phi)
-        factor = -(c1 * sin
-                   + 2 * c2 * cos * sin
-                   + 3 * c3 * cos2 * sin
-                   + 4 * c4 * cos3 * sin)
+        ### (-dE / d phi)
+        factor = (c1 * sin
+                  + 2 * c2 * cos * sin
+                  + 3 * c3 * cos2 * sin
+                  + 4 * c4 * cos3 * sin)
 
         ### (d phi / d ri) copied from OpenMM
         rsq2 = ew_dot(vec2, vec2)
         r2 = np.sqrt(rsq2)
 
-        factor1 = - factor * r2 / rsq_n1
-        factor4 = factor * r2 / rsq_n2
+        factor1 = factor * r2 / rsq_n1
+        factor4 = -factor * r2 / rsq_n2
 
         forces_a1 = factor1[:, np.newaxis] * n1
         forces_a4 = factor4[:, np.newaxis] * n2
@@ -173,7 +175,7 @@ class PeriodicTorsionKernel():
 
 class HarmonicTorsionKernel():
     '''
-    E = k (phi-phi0)^2
+    E = 0.5 * k (phi-phi0)^2
 
     '''
 
@@ -185,13 +187,13 @@ class HarmonicTorsionKernel():
         self.a3 = self.indexes[:, 2]
         self.a4 = self.indexes[:, 3]
         self.parameters = np.array(parameters, dtype=np.float64)
-        self.phi0 = self.parameters[:, 0] / 180 * np.pi
+        self.phi0 = self.parameters[:, 0]
         self.k = self.parameters[:, 1]
 
     def evaluate(self):
-        vec1 = self.positions[self.a2] - self.positions[self.a1]
+        vec1 = self.positions[self.a1] - self.positions[self.a2]
         vec2 = self.positions[self.a3] - self.positions[self.a2]
-        vec3 = self.positions[self.a4] - self.positions[self.a3]
+        vec3 = self.positions[self.a3] - self.positions[self.a4]
         n1 = np.cross(vec1, vec2)
         n2 = np.cross(vec2, vec3)
         rsq_n1 = ew_dot(n1, n1)
@@ -208,10 +210,11 @@ class HarmonicTorsionKernel():
         d_phi_min = np.min([d_phi_abs, d_phi_2pi], axis=0)
         energy = 0.5 * self.k * d_phi_min ** 2
 
+        ### (-dE / d phi)
         factor = -self.k * d_phi_min
 
         sign.fill(1)
-        sign[d_phi_abs > np.pi] = -1
+        sign[d_phi_2pi < d_phi_abs] = -1
         factor *= sign
 
         sign.fill(1)
@@ -222,8 +225,75 @@ class HarmonicTorsionKernel():
         rsq2 = ew_dot(vec2, vec2)
         r2 = np.sqrt(rsq2)
 
-        factor1 = - factor * r2 / rsq_n1
-        factor4 = factor * r2 / rsq_n2
+        factor1 = factor * r2 / rsq_n1
+        factor4 = -factor * r2 / rsq_n2
+
+        forces_a1 = factor1[:, np.newaxis] * n1
+        forces_a4 = factor4[:, np.newaxis] * n2
+
+        factor2 = ew_dot(vec1, vec2) / rsq2
+        factor3 = ew_dot(vec3, vec2) / rsq2
+
+        s = factor2[:, np.newaxis] * forces_a1 - factor3[:, np.newaxis] * forces_a4
+
+        forces_a2 = -forces_a1 + s
+        forces_a3 = -forces_a4 - s
+        ###
+
+        forces = np.zeros(self.positions.shape, dtype=np.float64)
+        # TODO vectorize this loop
+        for i in range(self.positions.shape[0]):
+            forces[i] += forces_a1[self.a1 == i].sum(axis=0)
+            forces[i] += forces_a2[self.a2 == i].sum(axis=0)
+            forces[i] += forces_a3[self.a3 == i].sum(axis=0)
+            forces[i] += forces_a4[self.a4 == i].sum(axis=0)
+
+        return phi, energy, forces
+
+
+class ConstrainedTorsionKernel():
+    '''
+    E = k (1-cos(phi-phi0))
+
+    '''
+
+    def __init__(self, positions, indexes, parameters):
+        self.positions = np.array(positions, dtype=np.float64)
+        self.indexes = np.array(indexes, dtype=int)
+        self.a1 = self.indexes[:, 0]
+        self.a2 = self.indexes[:, 1]
+        self.a3 = self.indexes[:, 2]
+        self.a4 = self.indexes[:, 3]
+        self.parameters = np.array(parameters, dtype=np.float64)
+        self.phi0 = self.parameters[:, 0]
+        self.k = self.parameters[:, 1]
+
+    def evaluate(self):
+        vec1 = self.positions[self.a1] - self.positions[self.a2]
+        vec2 = self.positions[self.a3] - self.positions[self.a2]
+        vec3 = self.positions[self.a3] - self.positions[self.a4]
+        n1 = np.cross(vec1, vec2)
+        n2 = np.cross(vec2, vec3)
+        rsq_n1 = ew_dot(n1, n1)
+        rsq_n2 = ew_dot(n2, n2)
+        cos = ew_dot(n1, n2) / np.sqrt(rsq_n1 * rsq_n2)
+        np.clip(cos, -1, 1, out=cos)
+        sign = np.ones(vec1.shape[0])
+        sign[ew_dot(vec1, n2) < 0] = -1
+        phi = sign * np.arccos(cos)
+
+        d_phi = phi - self.phi0
+        energy = self.k * (1 - np.cos(d_phi))
+
+        ### (-dE / d phi)
+        factor = -self.k * np.sin(d_phi)
+
+        ### (d phi / d ri) copied from OpenMM
+        rsq2 = ew_dot(vec2, vec2)
+        r2 = np.sqrt(rsq2)
+
+        factor1 = factor * r2 / rsq_n1
+        factor4 = -factor * r2 / rsq_n2
 
         forces_a1 = factor1[:, np.newaxis] * n1
         forces_a4 = factor4[:, np.newaxis] * n2
@@ -262,7 +332,7 @@ class NonbondedKernel():
         self.c12 = 4 * self.parameters[:, 0] * self.parameters[:, 1] ** 12
         self.c6 = 4 * self.parameters[:, 0] * self.parameters[:, 1] ** 6
         self.qq = self.parameters[:, 2]
-        self.qqconv = 138.935455
+        self.qqconv = constant.ONE_4PI_EPS0
 
     def evaluate(self):
         delta = self.positions[self.a2] - self.positions[self.a1]

mstools/topology/connectivity.py
@@ -47,6 +47,17 @@ class Bond():
         return '%s-%s' % (self.atom1.name, self.atom2.name)
 
     @property
+    def id_atoms(self):
+        '''
+        The ids of atoms forming this bond
+
+        Returns
+        -------
+        id_tuple : tuple of int
+        '''
+        return self.atom1.id, self.atom2.id
+
+    @property
     def is_drude(self):
         '''
         Whether or not this bond is a Drude dipole bond
@@ -116,6 +127,17 @@ class Angle():
         '''
         return '%s-%s-%s' % (self.atom1.name, self.atom2.name, self.atom3.name)
 
+    @property
+    def id_atoms(self):
+        '''
+        The ids of atoms forming this angle
+
+        Returns
+        -------
+        id_tuple : tuple of int
+        '''
+        return self.atom1.id, self.atom2.id, self.atom3.id
+
     def evaluate(self):
         '''
         Evaluate the value of this angle
@@ -126,7 +148,7 @@ class Angle():
         '''
         vec1 = self.atom1.position - self.atom2.position
         vec2 = self.atom3.position - self.atom2.position
-        return np.arccos(vec1.dot(vec2) / np.sqrt(vec1.dot(vec1) * vec2.dot(vec2))) / np.pi * 180
+        return np.arccos(vec1.dot(vec2) / np.sqrt(vec1.dot(vec1) * vec2.dot(vec2)))
 
 
 class Dihedral():
@@ -180,6 +202,17 @@ class Dihedral():
         return '%s-%s-%s-%s' \
                % (self.atom1.name, self.atom2.name, self.atom3.name, self.atom4.name)
 
+    @property
+    def id_atoms(self):
+        '''
+        The ids of atoms forming this dihedral
+
+        Returns
+        -------
+        id_tuple : tuple of int
+        '''
+        return self.atom1.id, self.atom2.id, self.atom3.id, self.atom4.id
+
     def evaluate(self):
         '''
         Evaluate the value of this dihedral
@@ -193,7 +226,7 @@ class Dihedral():
         vec3 = self.atom4.position - self.atom3.position
         n1 = np.cross(vec1, vec2)
         n2 = np.cross(vec2, vec3)
-        value = np.arccos(n1.dot(n2) / np.sqrt(n1.dot(n1) * n2.dot(n2))) * 180 / np.pi
+        value = np.arccos(n1.dot(n2) / np.sqrt(n1.dot(n1) * n2.dot(n2)))
         sign = 1 if vec1.dot(n2) >= 0 else -1
         return sign * value
 
@@ -250,6 +283,17 @@ class Improper():
         '''
         return '%s-%s-%s-%s' % (self.atom1.name, self.atom2.name, self.atom3.name, self.atom4.name)
 
+    @property
+    def id_atoms(self):
+        '''
+        The ids of atoms forming this improper torsion
+
+        Returns
+        -------
+        id_tuple : tuple of int
+        '''
+        return self.atom1.id, self.atom2.id, self.atom3.id, self.atom4.id
+
     def evaluate(self):
         '''
         Evaluate the value of this improper torsion defined as the angle between plane a1-a2-a3 and a2-a3-a4
@@ -263,6 +307,6 @@ class Improper():
         vec3 = self.atom4.position - self.atom3.position
         n1 = np.cross(vec1, vec2)
         n2 = np.cross(vec2, vec3)
-        value = np.arccos(n1.dot(n2) / np.sqrt(n1.dot(n1) * n2.dot(n2))) * 180 / np.pi
+        value = np.arccos(n1.dot(n2) / np.sqrt(n1.dot(n1) * n2.dot(n2)))
         sign = 1 if vec1.dot(n2) >= 0 else -1
         return sign * value

tests/analyzer/test_energy_kernels.py
@@ -5,71 +5,35 @@ import itertools
 import tempfile
 import filecmp
 import pytest
+from simtk import openmm as mm
+from simtk import unit
 from mstools.topology import Topology, Molecule
 from mstools.analyzer.energy_kernels import *
-
-from simtk import openmm as mm
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 top = Topology.open(cwd + '/../topology/files/CH3NH2.pdb')
-system = mm.System()
-for i in range(top.n_atom):
-    system.addParticle(1)
 
-force = mm.HarmonicBondForce()
-force.setForceGroup(1)
-for i, bond in enumerate(top.bonds):
-    force.addBond(bond.atom1.id, bond.atom2.id, 0.1 + 0.01 * i, 100 + 10 * i)
-system.addForce(force)
 
-force = mm.HarmonicAngleForce()
-force.setForceGroup(2)
-for i, angle in enumerate(top.angles):
-    force.addAngle(angle.atom1.id, angle.atom2.id, angle.atom3.id, (120 + 12 * i) / 180 * np.pi, 100 + 10 * i)
-system.addForce(force)
+def calc_energy_with_omm(force, positions):
+    system = mm.System()
+    for i in range(len(positions)):
+        system.addParticle(1)
+    system.addForce(force)
 
-force = mm.PeriodicTorsionForce()
-force.setForceGroup(3)
-for i, dihedral in enumerate(top.dihedrals):
-    for k in range(1, 5):
-        force.addTorsion(dihedral.atom1.id, dihedral.atom2.id, dihedral.atom3.id, dihedral.atom4.id,
-                         k, ((k - 1) % 2) * np.pi, k + 0.1 * i)
-system.addForce(force)
+    integrator = mm.VerletIntegrator(0.001)
+    platform = mm.Platform.getPlatformByName('Reference')
+    context = mm.Context(system, integrator, platform)
+    context.setPositions(positions)
 
-force = mm.CustomTorsionForce('0.5*k*min(dtheta, 2*pi-dtheta)^2;'
-                              'dtheta = abs(theta-theta0);'
-                              'pi = 3.1415926535')
-force.addPerTorsionParameter('theta0')
-force.addPerTorsionParameter('k')
-force.setForceGroup(4)
-for i, dihedral in enumerate(top.dihedrals):
-    force.addTorsion(dihedral.atom1.id, dihedral.atom2.id, dihedral.atom3.id, dihedral.atom4.id,
-                     # [1 + 0.1 * i, 2 + 0.1 * i]
-                     [top.dihedrals[i].evaluate() * 0.8 / 180 * np.pi, 2 + 0.1 * i]
-                     )
-system.addForce(force)
-
-force = mm.NonbondedForce()
-force.setNonbondedMethod(mm.NonbondedForce.NoCutoff)
-force.setForceGroup(5)
-for i in range(top.n_atom):
-    force.addParticle(0, 1, 0)
-pairs = list(itertools.combinations(list(range(top.n_atom)), 2))
-parameters = [(i, j / 1000, i / 10 + j / 10) for i, j in pairs]
-for (i, j), parameter in zip(pairs, parameters):
-    force.addException(i, j, parameter[2], parameter[1], parameter[0])
-system.addForce(force)
-
-integrator = mm.VerletIntegrator(0.001)
-platform = mm.Platform.getPlatformByName('Reference')
-context = mm.Context(system, integrator, platform)
-context.setPositions(top.positions)
+    state = context.getState(getEnergy=True, getForces=True)
+    return state.getPotentialEnergy().value_in_unit_system(unit.md_unit_system), \
+           state.getForces(asNumpy=True).value_in_unit_system(unit.md_unit_system)
 
 
 def test_harmonic_bond_kernel():
     kernel = HarmonicBondKernel(top.positions,
-                                [(bond.atom1.id, bond.atom2.id) for bond in top.bonds],
+                                [bond.id_atoms for bond in top.bonds],
                                 [[0.1 + 0.01 * i, 100 + 10 * i] for i in range(top.n_bond)],
                                 )
     r, energy, forces = kernel.evaluate()
@@ -79,15 +43,20 @@ def test_harmonic_bond_kernel():
     print(sum(energy))
     print(forces)
 
-    state = context.getState(getEnergy=True, getForces=True, groups={1})
-    print(state.getPotentialEnergy())
-    print(state.getForces(asNumpy=True))
+    force = mm.HarmonicBondForce()
+    force.setForceGroup(1)
+    for i, bond in enumerate(top.bonds):
+        force.addBond(*bond.id_atoms, 0.1 + 0.01 * i, 100 + 10 * i)
+    e, f = calc_energy_with_omm(force, top.positions)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f
 
 
 def test_harmonic_angle_kernel():
     kernel = HarmonicAngleKernel(top.positions,
-                                 [(angle.atom1.id, angle.atom2.id, angle.atom3.id) for angle in top.angles],
-                                 [[120 + 12 * i, 100 + 10 * i] for i in range(top.n_angle)],
+                                 [angle.id_atoms for angle in top.angles],
+                                 [[1 + 0.1 * i, 100 + 10 * i] for i in range(top.n_angle)],
                                  )
     theta, energy, forces = kernel.evaluate()
     print()
@@ -96,65 +65,104 @@ def test_harmonic_angle_kernel():
     print(sum(energy))
     print(forces)
 
-    state = context.getState(getEnergy=True, getForces=True, groups={2})
-    print(state.getPotentialEnergy())
-    print(state.getForces(asNumpy=True))
+    force = mm.HarmonicAngleForce()
+    force.setForceGroup(2)
+    for i, angle in enumerate(top.angles):
+        force.addAngle(*angle.id_atoms, 1 + 0.1 * i, 100 + 10 * i)
+    e, f = calc_energy_with_omm(force, top.positions)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f
 
 
-def test_periodic_torsion_kernel():
-    pos = top.positions + np.random.random(top.positions.shape)
-
-    kernel = PeriodicTorsionKernel(pos,
-                                   [(dihedral.atom1.id, dihedral.atom2.id, dihedral.atom3.id, dihedral.atom4.id)
-                                    for dihedral in top.dihedrals],
-                                   [[1 + 0.1 * i, 2 + 0.1 * i, 3 + 0.1 * i, 4 + 0.1 * i]
-                                    # [[1 + 0.1 * i, 0, 0, 0]
-                                    for i in range(top.n_dihedral)],
-                                   )
+def test_opls_torsion_kernel():
+    kernel = OplsTorsionKernel(top.positions,
+                               [dihedral.id_atoms for dihedral in top.dihedrals],
+                               [[1 + 0.1 * i, 2 + 0.1 * i, 3 + 0.1 * i, 4 + 0.1 * i]
+                                for i in range(top.n_dihedral)],
+                               )
     phi, energy, forces = kernel.evaluate()
     print()
-    for d, v in zip(top.dihedrals, phi * 180 / np.pi):
-        print(d.atom1.id + 1, d.atom2.id + 1, d.atom3.id + 1, d.atom4.id + 1, v)
+    for dihedral, val in zip(top.dihedrals, phi):
+        print([i + 1 for i in dihedral.id_atoms], val * 180 / np.pi)
     print(energy)
     print(sum(energy))
     print(forces)
 
-    context.setPositions(pos)
-    state = context.getState(getEnergy=True, getForces=True, groups={3})
-    print(state.getPotentialEnergy())
-    print(state.getForces(asNumpy=True))
+    force = mm.PeriodicTorsionForce()
+    force.setForceGroup(3)
+    for i, dihedral in enumerate(top.dihedrals):
+        for k in range(1, 5):
+            force.addTorsion(*dihedral.id_atoms, k, ((k - 1) % 2) * np.pi, k + 0.1 * i)
+    e, f = calc_energy_with_omm(force, top.positions)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f
 
 
 def test_harmonic_torsion_kernel():
-    pos = top.positions + np.random.random(top.positions.shape)
-    print([[top.dihedrals[i].evaluate(), 2 + 0.1 * i]
-                                    for i in range(top.n_dihedral)])
-
-    kernel = HarmonicTorsionKernel(pos,
-                                   [(dihedral.atom1.id, dihedral.atom2.id, dihedral.atom3.id, dihedral.atom4.id)
-                                    for dihedral in top.dihedrals],
-                                   # [[1 + 0.1 * i, 2 + 0.1 * i] for i in range(top.n_dihedral)],
-                                   [[top.dihedrals[i].evaluate()*0.8, 2 + 0.1 * i]
-                                    for i in range(top.n_dihedral)]
+    kernel = HarmonicTorsionKernel(top.positions,
+                                   [dihedral.id_atoms for dihedral in top.dihedrals],
+                                   [[1 + 0.1 * i, 2 + 0.1 * i] for i in range(top.n_dihedral)]
                                    )
     phi, energy, forces = kernel.evaluate()
     print()
-    print(phi)
+    print(phi * 180 / np.pi)
     print(energy)
     print(sum(energy))
     print(forces)
 
-    context.setPositions(pos)
-    state = context.getState(getEnergy=True, getForces=True, groups={4})
-    print(state.getPotentialEnergy())
-    print(state.getForces(asNumpy=True))
+    force = mm.CustomTorsionForce('0.5*k*min(2*pi-dtheta, dtheta)^2;'
+                                  'dtheta=abs(theta-theta0);'
+                                  'pi=3.1415926535')
+    force.addPerTorsionParameter('theta0')
+    force.addPerTorsionParameter('k')
+    force.setForceGroup(4)
+    for i, dihedral in enumerate(top.dihedrals):
+        force.addTorsion(*dihedral.id_atoms, [1 + 0.1 * i, 2 + 0.1 * i])
+
+    e, f = calc_energy_with_omm(force, top.positions)
+    print(e)
+    print(f)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f
+
+
+def test_constrained_torsion_kernel():
+    kernel = ConstrainedTorsionKernel(top.positions,
+                                      [dihedral.id_atoms for dihedral in top.dihedrals],
+                                      [[1 + 0.1 * i, 2 + 0.1 * i] for i in range(top.n_dihedral)]
+                                      )
+    phi, energy, forces = kernel.evaluate()
+    print()
+    print(phi * 180 / np.pi)
+    print(energy)
+    print(sum(energy))
+    print(forces)
+
+    force = mm.CustomTorsionForce('k*(1-cos(theta-theta0))')
+    force.addPerTorsionParameter('theta0')
+    force.addPerTorsionParameter('k')
+    force.setForceGroup(4)
+    for i, dihedral in enumerate(top.dihedrals):
+        force.addTorsion(*dihedral.id_atoms, [1 + 0.1 * i, 2 + 0.1 * i])
+
+    e, f = calc_energy_with_omm(force, top.positions)
+    print(e)
+    print(f)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f
 
 
 def test_nonbonded_kernel():
     pairs = list(itertools.combinations(list(range(top.n_atom)), 2))
     parameters = [(i, j / 1000, i / 10 + j / 10) for i, j in pairs]
 
-    kernel = NonbondedKernel(top.positions, pairs, parameters)
+    kernel = NonbondedKernel(top.positions,
+                             pairs,
+                             parameters)
     r, energy, forces = kernel.evaluate()
     print()
     print(r)
@@ -162,6 +170,14 @@ def test_nonbonded_kernel():
     print(sum(energy))
     print(forces)
 
-    state = context.getState(getEnergy=True, getForces=True, groups={5})
-    print(state.getPotentialEnergy())
-    print(state.getForces(asNumpy=True))
+    force = mm.NonbondedForce()
+    force.setNonbondedMethod(mm.NonbondedForce.NoCutoff)
+    force.setForceGroup(5)
+    for i in range(top.n_atom):
+        force.addParticle(0, 1, 0)
+    for (i, j), parameter in zip(pairs, parameters):
+        force.addException(i, j, parameter[2], parameter[1], parameter[0])
+    e, f = calc_energy_with_omm(force, top.positions)
+
+    assert pytest.approx(sum(energy), rel=1E-6) == e
+    assert pytest.approx(forces, rel=1E-6) == f