216071b5539d16288c9b9971a80076d180801346
Zheng Gong <z.gong@outlook.com>
2024-05-25 12:40:25 +0800

    release 0.3.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ scripts = [os.path.join(dir_scripts, f) for f in os.listdir(dir_scripts)]
 
 setuptools.setup(
     name='mstk',
-    version='0.3.12',
+    version='0.3.13',
     author='Zheng Gong',
     author_email='z.gong@outlook.com',
     description='Molecular simulation toolkit',
