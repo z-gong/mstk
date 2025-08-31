b466b63f7af3554a18bcffeda4e610084032036e
Zheng Gong <z.gong@outlook.com>
2025-08-31 20:21:50 +0800

    GAFF typer improved

--- mstk/data/forcefield/gaff.smt
+++ mstk/data/forcefield/gaff.smt
@@ -1,15 +1,16 @@
-## This is an unofficial implementation of GAFF atom types based on Table 1 in the following article:
+## This is an unofficial implementation of GAFF atom types based on AmberTools results and Table 1 in the following article:
 ## * Wang, J.; Wolf, R. M.; Caldwell, J. W.; Kollman, P. A.; Case, D. A. Development and Testing of a General Amber Force Field. Journal of Computational Chemistry 2004, 25 (9), 1157–1174. https://doi.org/10.1002/jcc.20035.
-## It covers the 35 basic atom types and 23 special atom types.
+## It covers the 35 basic atom types and 23 special atom types, plus atom type `s` and `hx`, not mentioned in the paper.
 ## Because of the special treatment for conjugated atoms, it requires GaffTyper typing engine.
 
-## This definition does not intend to give exact results as the official implementation in AmberTools.
-## Actually, AmberTools itself does not strictly follow the description in the paper either, especially for types 'na', 'nh' and 'h4'.
-## E.g. try molecules 'NC=C', 'C1=CC=C1N(C)', 'C1=CC=CO1' and check the types assigned for N, O and H atoms.
-## A major discrepancy between this implementation and AmberTools is on aromatic compounds.
-## RDKit is used here for determining aromaticity, while AmberTools has its own criterion.
-## To name one, GAFF does not consider 5-member rings as aromatic.
-## Therefore, this definition and the parameters in `gaff.zff` are mainly used as an initial guess for refining a force field.
+## AmberTools does not strictly follow the description in the paper, especially for types 'na', 'nh', 's' and 'ha'.
+## E.g. try molecules 'NC=C', 'C1=CC=C1N(C)', 'C1=CC=CO1' and check the types assigned for N and H atoms.
+## When Antechamber disagrees with GAFF paper, this implementation follows AmberTools.
+
+## A major discrepancy between this implementation and AmberTools is on 5-member aromatic rings.
+## RDKit is used here for determining aromaticity, while AmberTools does not consider 5-member rings as aromatic.
+## For carbon atoms in pyrrole and furan, AmberTools assign conjugated ring types, while this implementation will assign aromatic types.
+## The aromatic atom types are mainly used to maintain the planer structure of aromatic rings. The definition here should work well for that purpose.
 
 ## Hybridization assignment is nasty. E.g. N in N-c1ccccc1 is SP2 hybridized, whereas P in P-c1ccccc1 is SP3 hybridized.
 ## N in N-C=C is considered as SP2 hybridized by RDKit, but it is not a strong planer structure.
@@ -24,18 +25,19 @@ c1     [C;X2]                   ## SP1 C
 c2     [C;X3]                   ## SP2 C, aliphatic
 c3     [C;X4]                   ## SP3 C
 ca     [c]                      ## SP2 C, aromatic
-n      [N;X3][C,S,P]=[O,S]      ## amide. Cannot use hybridization. RDKit treat N in NC=O as SP2, but in NS=O as SP3
+n      [N;X3]C=[O,S]            ## amide
 n1     [N^1;X1]                 ## SP1 nitrogen. E.g. nitrile
 n2     [N^2;X2]                 ## SP2 nitrogen with 2 coordinates and a double bond. E.g. imine
 n3     [N^3;X3]                 ## SP3 nitrogen with 3 coordinates. E.g. amine
 n4     [N^3;X4]                 ## SP3 nitrogen with 4 coordinates. E.g. ammonium
-na     [N^2;X3]                 ## SP2 nitrogen with 3 coordinates. This type is ill defined. RDKit considers N in NC=C as SP2
+na     [#7^2;X3]                ## SP2 nitrogen with 3 coordinates. This type is ill defined. RDKit considers N in NC=C as SP2
 nh     [N;X3](*)(*)[a]          ## amine connected to aromatic. This type is well defined but ill implemented in AmberTools
 no     [N;X3](=O)(~O)           ## nitro group
 o      [O;X1]                   ## O in carbonyl or nitro group
 oh     [O;X2;H]
 os     [#8;X2;H0]               ## there is no aromatic O in GAFF
-s2     [S;X1]=*
+s      [S;X1]                   ## non-existent in GAFF paper
+s2     [S;X2]=,#*               ## different from the definition in GAFF paper
 sh     [S;X2;H]
 ss     [#16;X2;H0]              ## there is no aromatic S in GAFF
 s4     [S;X3]=*
@@ -44,8 +46,9 @@ p2     [P;X2]=*
 p3     [P;X3](*)(*)*            ## three coordinates without double bond. P in P-c1ccccc1 is SP3 hybridized
 p4     [P;X3]=*
 p5     [P;X4]=*
-hc     [H]C
-ha     [H]c
+hx     [H][#6][N+;X4]           ## non-existent in GAFF paper
+hc     [H][C;X4]
+ha     [H][#6;X2,X3]            ## H attached to SP2 or SP3 C, not just aromatic C as described in GAFF paper
 hn     [H][#7]
 ho     [H][#8]
 hs     [H][#16]
@@ -62,7 +65,7 @@ cu     [C;X3;r3]
 cv     [C;X3;r4]
 cx     [C;X4;r3]
 cy     [C;X4;r4]
-nb     [n]
+nb     [n;X2]                   ## AmberTools only consider two-coordinated N as aromatic
 nc|nd  [N;X2](=;@*)-;@*!-;@*
 ne|nf  [N;X2](=*)-*!-*
 pb     [p]
@@ -72,11 +75,11 @@ px     [P;X3](=*)-*!-*
 py     [P;X4](=*)-*!-*
 sx     [S;X3](=*)-*!-*
 sy     [S;X4](=*)-*!-*
-h1     [H]C~[#7,#8,F,Cl,Br]
-h2     [H]C(~[#7,#8,F,Cl,Br])~[#7,#8,F,Cl,Br]
-h3     [H]C(~[#7,#8,F,Cl,Br])(~[#7,#8,F,Cl,Br])~[#7,#8,F,Cl,Br]
-h4     [H]c~[#7,#8,F,Cl,Br]
-h5     [H]c(~[#7,#8,F,Cl,Br])~[#7,#8,F,Cl,Br]
+h1     [H]C~[#7,#8,#16,F,Cl,Br,I]
+h2     [H]C(~[#7,#8,#16,F,Cl,Br,I])~[#7,#8,#16,F,Cl,Br,I]
+h3     [H]C(~[#7,#8,#16,F,Cl,Br,I])(~[#7,#8,#16,F,Cl,Br,I])~[#7,#8,#16,F,Cl,Br,I]
+h4     [H][#6]~[#7,#8,#16,F,Cl,Br,I]
+h5     [H][#6](~[#7,#8,#16,F,Cl,Br,I])~[#7,#8,#16,F,Cl,Br,I]
 
 
 HierarchicalTree
@@ -107,6 +110,7 @@ na
 o
 oh
 os
+s
 s2
 sh
 ss
@@ -122,13 +126,14 @@ p4
     px
 p5
     py
+hx
+ha
+    h4
+        h5
 hc
     h1
         h2
             h3
-ha
-    h4
-        h5
 hn
 ho
 hs
--- tests/forcefield/test_gaff.py
+++ tests/forcefield/test_gaff.py
@@ -1,6 +1,4 @@
 import os
-import tempfile
-import filecmp
 import pytest
 
 from mstk.topology import Molecule, Topology
@@ -9,52 +7,102 @@ from mstk.simsys import System
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
+name_smiles_types_antechamber = [
+    ('a-01', 'CCCF', 'c3-c3-c3-f-hc-hc-hc-hc-hc-h1-h1'),
+    ('a-02', 'ClC=CBr', 'cl-c2-c2-br-h4-h4'),
+    ('a-03', 'COc1ccc(O)cc1', 'c3-os-ca-ca-ca-ca-oh-ca-ca-h1-h1-h1-ha-ha-ho-ha-ha'),
+    ('a-04', 'ICC=O', 'i-c3-c-o-h1-h1-h4'),
+    ('a-05', 'CC(=O)CS', 'c3-c-o-c3-sh-hc-hc-hc-h1-h1-hs'),
+    ('a-06', 'CC(=S)O', 'c3-c-s-oh-hc-hc-hc-ho'),
+    ('a-07', 'CSCC#C', 'c3-ss-c3-c1-c1-h1-h1-h1-h1-h1-ha'),
+    ('a-08', 'CSSC', 'c3-ss-ss-c3-h1-h1-h1-h1-h1-h1'),
+    ('a-09', 'CS(=O)C', 'c3-s4-o-c3-h1-h1-h1-h1-h1-h1'),
+    ('a-10', 'CS(=O)(=O)NC', 'c3-s6-o-o-n3-c3-h1-h1-h1-hn-h1-h1-h1'),
+    ('a-11', 'CC(=O)N', 'c3-c-o-n-hc-hc-hc-hn-hn'),
+    ('a-12', 'NCC=NO', 'n3-c3-c2-n2-oh-hn-hn-h1-h1-h4-ho'),
+    ('a-13', 'C[NH3+]', 'c3-n4-hx-hx-hx-hn-hn-hn'),
+    ('a-14', 'Cn1cccc1', 'c3-na-cc-cd-cd-cc-h1-h1-h1-h4-ha-ha-h4'),  # antechamber does not consider pyrrole as aromatic
+    ('a-15', 'c1ccccc1N', 'ca-ca-ca-ca-ca-ca-nh-ha-ha-ha-ha-ha-hn-hn'),
+    ('a-16', 'CP=CC[N+](=O)[O-]', 'c3-p2-c2-c3-no-o-o-hc-hc-hc-ha-h1-h1'),
+    ('a-17', 'CP', 'c3-p3-hc-hc-hc-hp-hp'),
+    ('a-18', 'CP(=O)C', 'c3-p5-o-c3-hc-hc-hc-hp-hc-hc-hc'),
+    ('a-19', 'CP(=O)(O)OC', 'c3-p5-o-oh-os-c3-hc-hc-hc-ho-h1-h1-h1'),
+    ('b-01', 'C=CC=C', 'c2-ce-ce-c2-ha-ha-ha-ha-ha-ha'),
+    ('b-02', 'C=CN=C', 'c2-ce-ne-c2-ha-ha-h4-h4-h4'),
+    ('b-03', 'C=CC=CC=CC=C', 'c2-ce-ce-cf-cf-ce-ce-c2-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-04', 'C#CC=C', 'c1-cg-ce-c2-ha-ha-ha-ha'),
+    ('b-05', 'C=CP=C', 'c2-ce-pe-c2-ha-ha-ha-ha-ha'),
+    ('b-06', 'o1cccc1', 'os-cc-cd-cd-cc-h4-ha-ha-h4'),  # antechamber does not consider furan as aromatic
+    ('b-07', 'S1-C=C-N=C1', 'ss-cc-cd-nd-cc-h4-h4-h5'),  # antechamber does not consider 5-member ring as aromatic
+    ('b-08', 'O1-C=P-C=C1', 'os-cc-pd-cd-cc-h4-ha-h4'),  # antechamber does not consider 5-member ring as aromatic
+    ('b-09', 'C1=CNC=CC1=O', 'cc-cd-na-cc-cd-c-o-ha-h4-hn-h4-ha'),  # antechamber does not consider it as aromatic
+    ('b-10', 'c1ccccc1c2ccccc2', 'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-11', 'c1ccccc1[n+]2ccccc2', 'ca-ca-ca-ca-ca-cp-na-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-h4-ha-ha-ha-h4'),
+    ('b-12', 'c1ccccc1c2ccccc2c3ccccc3',
+     'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-cq-cq-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-13', 'c1ccccc1c2ccccc2c3cccc(c4ccco4)c3',
+     'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-cq-cq-ca-ca-ca-ca-cc-cd-cd-cc-os-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-h4-ha'),
+    # antechamber does not consider furan as aromatic
+    ('b-14', 'c1cccc(cccc2)c12', 'ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-15', 'c1c(cccc3)c3cc(cccc2)c12', 'ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-16', 'CS(=C)C', 'c3-s4-c2-c3-h1-h1-h1-h4-h4-h1-h1-h1'),
+    ('b-17', 'CS(=O)C=C', 'c3-sx-o-ce-c2-h1-h1-h1-h4-ha-ha'),
+    ('b-18', 'C1CC1S(=O)(=O)C2CC=C2', 'cx-cx-cx-s6-o-o-cy-cy-cv-cv-hc-hc-hc-hc-h1-h1-hc-hc-ha-ha'),
+    ('b-19', 'C1=CC1S(=O)(=O)C=C', 'cu-cu-cx-sy-o-o-ce-c2-ha-ha-h1-h4-ha-ha'),
+    ('b-20', 'C[P](=S)O', 'c3-p4-s-oh-hc-hc-hc-ho'),
+    ('b-21', 'C[P](=O)C=C', 'c3-px-o-ce-c2-hc-hc-hc-ha-ha-ha'),
+    ('b-22', 'CP(=N)(OC)OC', 'c3-p5-n2-os-c3-os-c3-hc-hc-hc-hn-h1-h1-h1-h1-h1-h1'),
+    ('b-23', 'COP(=N)(OC)N=C', 'c3-os-py-n2-os-c3-ne-c2-h1-h1-h1-hn-h1-h1-h1-h4-h4'),
+    ('b-24', 'COP(=O)(OC)P(=O)(OC)OC', 'c3-os-py-o-os-c3-py-o-os-c3-os-c3-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1'),
+    ('b-25', 'COP(O)(OC)=P(O)(OC)OC', 'c3-os-p5-oh-os-c3-p5-oh-os-c3-os-c3-h1-h1-h1-ho-h1-h1-h1-ho-h1-h1-h1-h1-h1-h1'),
+]
+
 name_smiles_types = [
     ('a-01', 'CCCF', 'c3-c3-c3-f-hc-hc-hc-hc-hc-h1-h1'),
-    ('a-02', 'ClC=CBr', 'cl-c2-c2-br-h1-h1'),
+    ('a-02', 'ClC=CBr', 'cl-c2-c2-br-h4-h4'),
     ('a-03', 'COc1ccc(O)cc1', 'c3-os-ca-ca-ca-ca-oh-ca-ca-h1-h1-h1-ha-ha-ho-ha-ha'),
-    ('a-04', 'ICC=O', 'i-c3-c-o-hc-hc-h1'),  # antechamber type the last hydrogen as ha
-    ('a-05', 'CC(=O)CS', 'c3-c-o-c3-sh-hc-hc-hc-hc-hc-hs'),
-    ('a-06', 'CC(=S)O', 'c3-c-s2-oh-hc-hc-hc-ho'),
-    ('a-07', 'CSCC#C', 'c3-ss-c3-c1-c1-hc-hc-hc-hc-hc-hc'),
-    ('a-08', 'CSSC', 'c3-ss-ss-c3-hc-hc-hc-hc-hc-hc'),
-    ('a-09', 'CS(=O)C', 'c3-s4-o-c3-hc-hc-hc-hc-hc-hc'),
-    ('a-10', 'CS(=O)(=O)NC', 'c3-s6-o-o-n-c3-hc-hc-hc-hn-h1-h1-h1'),
+    ('a-04', 'ICC=O', 'i-c3-c-o-h1-h1-h4'),
+    ('a-05', 'CC(=O)CS', 'c3-c-o-c3-sh-hc-hc-hc-h1-h1-hs'),
+    ('a-06', 'CC(=S)O', 'c3-c-s-oh-hc-hc-hc-ho'),
+    ('a-07', 'CSCC#C', 'c3-ss-c3-c1-c1-h1-h1-h1-h1-h1-ha'),
+    ('a-08', 'CSSC', 'c3-ss-ss-c3-h1-h1-h1-h1-h1-h1'),
+    ('a-09', 'CS(=O)C', 'c3-s4-o-c3-h1-h1-h1-h1-h1-h1'),
+    ('a-10', 'CS(=O)(=O)NC', 'c3-s6-o-o-n3-c3-h1-h1-h1-hn-h1-h1-h1'),
     ('a-11', 'CC(=O)N', 'c3-c-o-n-hc-hc-hc-hn-hn'),
-    ('a-12', 'NCC=NO', 'n3-c3-c2-n2-oh-hn-hn-h1-h1-h1-ho'),
-    ('a-13', 'C[NH3+]', 'c3-n4-h1-h1-h1-hn-hn-hn'),
-    ('a-14', 'Cn1cccc1', 'c3-nb-ca-ca-ca-ca-h1-h1-h1-h4-ha-ha-h4'),  # antechamber does not consider pyrrole as aromatic
+    ('a-12', 'NCC=NO', 'n3-c3-c2-n2-oh-hn-hn-h1-h1-h4-ho'),
+    ('a-13', 'C[NH3+]', 'c3-n4-hx-hx-hx-hn-hn-hn'),
+    ('a-14', 'Cn1cccc1', 'c3-na-ca-ca-ca-ca-h1-h1-h1-h4-ha-ha-h4'),  # antechamber does not consider pyrrole as aromatic
     ('a-15', 'c1ccccc1N', 'ca-ca-ca-ca-ca-ca-nh-ha-ha-ha-ha-ha-hn-hn'),
-    ('a-16', 'CP=CCN(=O)=O', 'c3-p2-c2-c3-no-o-o-hc-hc-hc-hc-h1-h1'),
+    ('a-16', 'CP=CC[N+](=O)[O-]', 'c3-p2-c2-c3-no-o-o-hc-hc-hc-ha-h1-h1'),
     ('a-17', 'CP', 'c3-p3-hc-hc-hc-hp-hp'),
-    ('a-18', 'C[P](=O)C', 'c3-p4-o-c3-hc-hc-hc-hc-hc-hc'),
+    ('a-18', 'CP(=O)C', 'c3-p5-o-c3-hc-hc-hc-hp-hc-hc-hc'),
     ('a-19', 'CP(=O)(O)OC', 'c3-p5-o-oh-os-c3-hc-hc-hc-ho-h1-h1-h1'),
-    ('b-01', 'C=CC=C', 'c2-ce-ce-c2-hc-hc-hc-hc-hc-hc'),
-    ('b-02', 'C=CN=C', 'c2-ce-ne-c2-hc-hc-h1-h1-h1'),
-    ('b-03', 'C=CC=CC=CC=C', 'c2-ce-ce-cf-cf-ce-ce-c2-hc-hc-hc-hc-hc-hc-hc-hc-hc-hc'),
-    ('b-04', 'C#CC=C', 'c1-cg-ce-c2-hc-hc-hc-hc'),
-    ('b-05', 'C=CP=C', 'c2-ce-pe-c2-hc-hc-hc-hc-hc'),
+    ('b-01', 'C=CC=C', 'c2-ce-ce-c2-ha-ha-ha-ha-ha-ha'),
+    ('b-02', 'C=CN=C', 'c2-ce-ne-c2-ha-ha-h4-h4-h4'),
+    ('b-03', 'C=CC=CC=CC=C', 'c2-ce-ce-cf-cf-ce-ce-c2-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
+    ('b-04', 'C#CC=C', 'c1-cg-ce-c2-ha-ha-ha-ha'),
+    ('b-05', 'C=CP=C', 'c2-ce-pe-c2-ha-ha-ha-ha-ha'),
     ('b-06', 'o1cccc1', 'os-ca-ca-ca-ca-h4-ha-ha-h4'),  # antechamber does not consider furan as aromatic
-    ('b-07', 'S1-C=C-N=C1', 'ss-ca-ca-nb-ca-ha-h4-h4'),  # antechamber does not consider 5-member ring as aromatic
+    ('b-07', 'S1-C=C-N=C1', 'ss-ca-ca-nb-ca-h4-h4-h5'),  # antechamber does not consider 5-member ring as aromatic
     ('b-08', 'O1-C=P-C=C1', 'os-ca-pb-ca-ca-h4-ha-h4'),  # antechamber does not consider 5-member ring as aromatic
-    ('b-09', 'C1=CNC=CC1=O', 'ca-ca-nb-ca-ca-ca-o-ha-h4-hn-h4-ha'),  # antechamber does not consider it as aromatic
+    ('b-09', 'C1=CNC=CC1=O', 'ca-ca-na-ca-ca-ca-o-ha-h4-hn-h4-ha'),  # antechamber does not consider it as aromatic
     ('b-10', 'c1ccccc1c2ccccc2', 'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
-    ('b-11', 'c1ccccc1[n+]2ccccc2', 'ca-ca-ca-ca-ca-cp-nb-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-h4-ha-ha-ha-h4'),
+    ('b-11', 'c1ccccc1[n+]2ccccc2', 'ca-ca-ca-ca-ca-cp-na-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-h4-ha-ha-ha-h4'),
     ('b-12', 'c1ccccc1c2ccccc2c3ccccc3',
      'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-cq-cq-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
     ('b-13', 'c1ccccc1c2ccccc2c3cccc(c4ccco4)c3',
      'ca-ca-ca-ca-ca-cp-cp-ca-ca-ca-ca-cq-cq-ca-ca-ca-cp-cp-ca-ca-ca-os-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha-h4-ha'),
-    # antechamer does not consider furan as aromatic
+    # antechamber does not consider furan as aromatic
     ('b-14', 'c1cccc(cccc2)c12', 'ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha'),
     ('b-15', 'c1c(cccc3)c3cc(cccc2)c12', 'ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ca-ha-ha-ha-ha-ha-ha-ha-ha-ha-ha'),
-    ('b-16', 'CS(=C)C', 'c3-s4-c2-c3-hc-hc-hc-hc-hc-hc-hc-hc'),
-    ('b-17', 'CS(=O)C=C', 'c3-sx-o-ce-c2-hc-hc-hc-hc-hc-hc'),
-    ('b-18', 'C1CC1S(=O)(=O)C2CC=C2', 'cx-cx-cx-s6-o-o-cy-cy-cv-cv-hc-hc-hc-hc-hc-hc-hc-hc-hc-hc'),
-    ('b-19', 'C1=CC1S(=O)(=O)C=C', 'cu-cu-cx-sy-o-o-ce-c2-hc-hc-hc-hc-hc-hc'),
-    ('b-20', 'C[P](=S)O', 'c3-p4-s2-oh-hc-hc-hc-ho'),
-    ('b-21', 'C[P](=O)C=C', 'c3-px-o-ce-c2-hc-hc-hc-hc-hc-hc'),
+    ('b-16', 'CS(=C)C', 'c3-s4-c2-c3-h1-h1-h1-h4-h4-h1-h1-h1'),
+    ('b-17', 'CS(=O)C=C', 'c3-sx-o-ce-c2-h1-h1-h1-h4-ha-ha'),
+    ('b-18', 'C1CC1S(=O)(=O)C2CC=C2', 'cx-cx-cx-s6-o-o-cy-cy-cv-cv-hc-hc-hc-hc-h1-h1-hc-hc-ha-ha'),
+    ('b-19', 'C1=CC1S(=O)(=O)C=C', 'cu-cu-cx-sy-o-o-ce-c2-ha-ha-h1-h4-ha-ha'),
+    ('b-20', 'C[P](=S)O', 'c3-p4-s-oh-hc-hc-hc-ho'),
+    ('b-21', 'C[P](=O)C=C', 'c3-px-o-ce-c2-hc-hc-hc-ha-ha-ha'),
     ('b-22', 'CP(=N)(OC)OC', 'c3-p5-n2-os-c3-os-c3-hc-hc-hc-hn-h1-h1-h1-h1-h1-h1'),
-    ('b-23', 'COP(=N)(OC)N=C', 'c3-os-py-n2-os-c3-ne-c2-h1-h1-h1-hn-h1-h1-h1-h1-h1'),
+    ('b-23', 'COP(=N)(OC)N=C', 'c3-os-py-n2-os-c3-ne-c2-h1-h1-h1-hn-h1-h1-h1-h4-h4'),
     ('b-24', 'COP(=O)(OC)P(=O)(OC)OC', 'c3-os-py-o-os-c3-py-o-os-c3-os-c3-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1-h1'),
     ('b-25', 'COP(O)(OC)=P(O)(OC)OC', 'c3-os-p5-oh-os-c3-p5-oh-os-c3-os-c3-h1-h1-h1-ho-h1-h1-h1-ho-h1-h1-h1-h1-h1-h1'),
 ]
